210fe7177c5bcdc74fc4b2374045f03468520dcb
heiliuer <heiliuer@github.com>
2016-11-18 17:08:05 +0800

    fixed

---
+++ nginx 1.7.11.3 Gryphon/vod_server/refresh_files/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+CONFIGS = {
+    'FILE_JSON': "../www/files.json",
+
+    'FILE_CONFIG': "folders.ini",
+
+    'FILE_FFMPEG': "../../ffmpeg",
+
+    'FILE_NGINX_CONFIG': "../confs/datas.conf"
+}
--- nginx 1.7.11.3 Gryphon/vod_server/refresh_files/gen.py
+++ nginx 1.7.11.3 Gryphon/vod_server/refresh_files/gen.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import json
-import uuid
 import hashlib
+import json
+import os
+import config
 
+FILE_JSON = config.CONFIGS['FILE_JSON']
 
-FILE_JSON = "../www/files.json"
+FILE_CONFIG = config.CONFIGS['FILE_CONFIG']
 
-FILE_CONFIG = "folders.ini"
+FILE_FFMPEG = config.CONFIGS['FILE_FFMPEG']
+
+FILE_NGINX_CONFIG = config.CONFIGS['FILE_NGINX_CONFIG']
 
 md5 = hashlib.md5()
+
 
 def read_folder_config(file_config):
     folders = []
@@ -21,6 +25,37 @@ def read_folder_config(file_config):
             if len(line) > 0:
                 folders.append(line)
     return folders
+
+
+def create_ffmpeg_img(video_file, img_path, size=(300, 200)):
+    """
+    生成FFmpeg预览图片
+    :param video_file:
+    :param img_root:
+    :param size:
+    :return:
+    """
+    size_str = 'x'.join(str(x) for x in size)
+
+    parent_path = os.path.dirname(img_path)
+    if not os.path.isdir(parent_path):
+        os.mkdir(parent_path)
+    video_file = os.path.abspath(video_file)
+    img_path = os.path.abspath(img_path)
+
+    if not os.path.isfile(img_path):
+        cwd = os.getcwd()
+        dirname = os.path.dirname(FILE_FFMPEG)
+        dir_valid = len(dirname) > 0
+        ffmpeg = FILE_FFMPEG
+        if dir_valid:
+            os.chdir(dirname)
+            ffmpeg = os.path.basename(FILE_FFMPEG)
+        cmd = ffmpeg + " -itsoffset -1 -i \"{}\" -vcodec mjpeg -vframes 1 -an -f rawvideo -s {} \"{}\"".format(
+            video_file, size_str, img_path)
+        os.system(cmd)
+        if dir_valid:
+            os.chdir(cwd)
 
 
 def walk_files_by_suffix(folder, suffixs):
@@ -44,8 +79,26 @@ def walk_files_by_suffix(folder, suffixs):
                 rpath = rt.replace(folder, '')
                 if len(rpath) > 0 and rpath[0] == os.path.sep:
                     rpath = rpath[1:]
-                # print(rpath, os.path.join(rpath, f))
-                relativeFiles.append(os.path.join(rpath, f))
+
+                absolute_file = os.path.join(rt, f)
+                uid = str(md5.hexdigest())
+
+                img_file = uid + '.png'
+                img_path = os.path.join("images", img_file)
+
+                create_ffmpeg_img(absolute_file, img_path)
+
+                md5.update(absolute_file.encode("utf-8"))
+
+                data = {
+                    'uid': uid,
+                    'path': os.path.join(rpath, f),
+                    'name': fname[0],
+                    'suffix': suffix,
+                    'ctime': int(os.stat(absolute_file).st_ctime) * 1000,
+                    'thumb_path': img_path
+                }
+                relativeFiles.append(data)
 
     return relativeFiles
 
@@ -59,25 +112,35 @@ def handle(suffixs, file_config=FILE_CONFIG, file_json=FILE_JSON):
         size += len(files)
         if len(files) > 0:
             md5.update(folder.encode("utf-8"))
-            data.append({'folder': folder, 'files': files, 'uid': str(md5.hexdigest())})
+            data.append(
+                {'folder': folder, 'name': os.path.basename(folder), 'files': files,
+                 'uid': str(md5.hexdigest())})
     write_json(data, file_json)
     print("files total count:%d" % size)
     return data
 
 
 def write_json(data, file_json=FILE_JSON):
-    rs_json = json.dumps(data, separators=(',', ':'))
+    rs_json = json.dumps(data, indent=4, separators=(',', ':'), )
     f = open(file_json, 'w')
     f.write(rs_json)
 
-if __name__ == "__main__":
-    datas = handle(["mp4", "mp3"])
-    f = open("../confs/datas.conf", 'w', encoding="utf-8")
+
+def generate_nginx_conf(datas, file_nginx_config=FILE_NGINX_CONFIG):
+    f = open(file_nginx_config, 'w', encoding="utf-8")
     f.write('#generated file,do not edit\n')
     for data in datas:
-        # print(data['uid'], data['folder'])
         f_str = '''
-    location /{} {{
+        location /{} {{
         alias {};
-    }}'''
+        }}'''
         f.write(f_str.format(data['uid'], data['folder']))
+
+
+def main():
+    data = handle(["mp4"])
+    generate_nginx_conf(data)
+
+
+if __name__ == "__main__":
+    main()
